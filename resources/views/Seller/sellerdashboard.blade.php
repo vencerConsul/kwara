33aa0014df910e4b18a42c96a01465b188ac5ce1
vencer <o.lermovenz@gmail.com>
2021-01-11 03:27:21 +0800

    initial commit

--- resources/views/Seller/sellerdashboard.blade.php
+++ resources/views/Seller/sellerdashboard.blade.php
@@ -160,7 +160,7 @@ Dashboard
 <div class="wrapper">
     <!-- Navbar -->
     <nav class="main-header navbar navbar-expand navbar-white navbar-light sticky-top">
-        <!-- Left navbar links -->
+        <!-- Left navbar linkss -->
         <ul class="navbar-nav">
             <li class="nav-item">
                 <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-align-left"></i></a>
