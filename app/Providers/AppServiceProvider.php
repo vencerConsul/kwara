a610a3ba00e5e837fb07def93219abe2aa9135f1
vencer <o.lermovenz@gmail.com>
2021-01-10 21:21:32 +0800

    initial commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,12 +2,10 @@
 
 namespace App\Providers;
 
+use Illuminate\Routing\UrlGenerator;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
-if (env('APP_ENV') !== 'local') {
-    $url->forceSchema('https');
-}
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -25,8 +23,12 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(UrlGenerator $url)
     {
         Schema::defaultStringLength(191);
+
+        if (env('APP_ENV') !== 'local') {
+            $url->forceSchema('https');
+        }
     }
 }
