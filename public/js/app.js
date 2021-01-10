d70bc7538e64d504e05031f343ab448fab229b82
vencer <o.lermovenz@gmail.com>
2021-01-10 22:13:18 +0800

    small changes

--- public/js/app.js
+++ public/js/app.js
@@ -12881,8 +12881,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\xampp\htdocs\sites\gotchu\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\xampp\htdocs\sites\gotchu\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\xampp\htdocs\sites\kwara\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\sites\kwara\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
